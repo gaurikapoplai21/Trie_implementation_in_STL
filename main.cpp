bd8592465a31a872def107bc3429d68c6604758b
PrithwishNag <prithwish.nag@gmail.com>
2021-05-04 12:24:22 -0700

    final

--- main.cpp
+++ main.cpp
@@ -27,27 +27,30 @@ int main()
     vector<string> x = t.autocomplete("z");
 
     cout << "Auto-complete result \'v\':" << endl;
-    for (auto i : x)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-
-    cout << "Auto-complete result \'x\':" << endl;
     for (auto a : v)
     {
         cout << a << " ";
     }
     cout << endl;
+    cout << endl;
 
-    cout << "T1:" << endl;
+    cout << "Auto-complete result \'x\':" << endl;
+    for (auto i : x)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "T1 Forward:" << endl;
     for (auto a : t1)
     {
         cout << a << " ";
     }
     cout << endl;
+    cout << endl;
 
-    cout << "Reverse: " << endl;
+    cout << "T Reverse: " << endl;
     it = t.end();
     for (; it != t.begin(); --it)
     {
@@ -55,22 +58,29 @@ int main()
             cout << *it << " ";
     }
     cout << *it << endl;
+    cout << endl;
 
     cout << "STD::Find" << endl;
     cout << (find(t.begin(), t.end(), "helluzb") != t.end()) << endl;
+    cout << endl;
 
     cout << "STD::equal" << endl;
     cout << equal(t.begin(), t.end(), t1.begin()) << endl;
+    cout << endl;
 
     cout << "begin(t):" << endl;
     cout << *begin(t) << endl;
+    cout << endl;
 
     cout << "end(t)-1:" << endl;
     cout << *(--end(t)) << endl;
+    cout << endl;
 
     cout << "begin == end:" << endl;
     vector<int> tx;
     cout << (tx.begin() == tx.end()) << endl;
+    cout << endl;
 
+    t.printTree();
     return 0;
 }
